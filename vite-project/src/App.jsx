8ca5607ff5586737b966cd038804b3eecc8e518a
nukri060 <nukri060@gmail.com>
2025-07-26 13:35:16 +0400

    refactor: extract header into separate component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <div>
-      <header>
-        <div className="logo">
-          <h1>DevConnect</h1>
-        </div>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/features">Features</a>
-          <a href="/blog">Blog</a>
-          <a href="/about">About</a>
-        </nav>
-      </header>
+      <Header/>
 
 
       <main>
